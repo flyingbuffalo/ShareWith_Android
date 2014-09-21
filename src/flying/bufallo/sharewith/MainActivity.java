4de46f501a091d7e652dd319c205edd0dafb0e93
Shin JJang <makao.rule@gmail.com>
2014-09-21 18:57:36 +0900

    누르면 순차적으로(진짜 고생함) 증가하는 원들. MAX_DEVICES 값을 바꾸면 적당히 나누어 위치됨.

--- src/flying/bufallo/sharewith/MainActivity.java
+++ src/flying/bufallo/sharewith/MainActivity.java
@@ -1,5 +1,12 @@
 package flying.bufallo.sharewith;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -9,7 +16,6 @@ import android.view.View.OnClickListener;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -21,7 +27,9 @@ public class MainActivity extends Activity {
 	float dpWidth;
 	
 	RelativeLayout main;
-	ImageView btnCenter;
+	ImageView btnCenter;	
+	AnimatorSet animatorSet = new AnimatorSet();
+	List<ObjectAnimator> aniList = new ArrayList<ObjectAnimator>();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +60,7 @@ public class MainActivity extends Activity {
 		fadeOut.setInterpolator(new AccelerateInterpolator()); // and this
 		fadeOut.setStartOffset(0);
 		fadeOut.setDuration(500);
-		btnCenter.setAnimation(fadeOut);
+		btnCenter.startAnimation(fadeOut);
 		
 		float x = btnCenter.getX();
 		float y = btnCenter.getY();
@@ -66,14 +74,19 @@ public class MainActivity extends Activity {
 			createdCircle(circle_x, circle_y, i);
 			
 			Log.d("TEST ANI", "circle " + i + " position : " + circle_x + " , " + circle_y);
-		}
+		}		
+		startFadeIn();
 	}
 	
+	// 원 위치에 그리고 애니메이션 + 뷰 = > 애니메이터를 만들고 저장
 	public void createdCircle(double x, double y, final int index) {
-		View child = new View(getApplicationContext());
+		final View child = new View(getApplicationContext());
 		child.setBackground(getResources().getDrawable(R.drawable.circle));		
 		child.setX((float) x);
 		child.setY((float) y);
+		child.setVisibility(View.GONE);
+		
+		Log.d("TEST ANI", "index is " + index);	
 		
 		child.setOnClickListener(new OnClickListener() {
 			
@@ -83,12 +96,45 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-		main.addView(child, 100, 100);
-		
-		Animation fadeIn_first = new AlphaAnimation(0, 1);
-		fadeIn_first.setInterpolator(new DecelerateInterpolator()); // add this
-		fadeIn_first.setDuration(1000);
+		ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(child, View.ALPHA, 0,1);
+		alphaAnimation.setDuration(1000);
+		alphaAnimation.addListener(new AnimatorListener() {
 			
-		child.setAnimation(fadeIn_first);		
+			@Override
+			public void onAnimationStart(Animator animation) {
+				child.setVisibility(View.VISIBLE);				
+			}
+			
+			@Override
+			public void onAnimationRepeat(Animator animation) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				//child.setVisibility(View.GONE);
+				
+			}
+			
+			@Override
+			public void onAnimationCancel(Animator animation) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		aniList.add(alphaAnimation);	
+		main.addView(child, 100, 100);				
 	}
+	
+	// 애니메이터 리스트에서 순차적으로 애니메이션의 순서를 정해줌. 그리고 시작.
+	public void startFadeIn() {
+		for(int i = 0; i < aniList.size() - 1; i++) {
+			animatorSet.play(aniList.get(i)).before(aniList.get(i+1));
+		}
+		animatorSet.start();
+	}
+	
+	
 }
