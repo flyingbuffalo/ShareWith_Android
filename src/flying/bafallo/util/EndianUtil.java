1d2dba5637431fb8cc8d79bb08aa6c23785a54bc
Shin JJang <makao.rule@gmail.com>
2014-11-12 19:36:12 +0900

    ByteBuffer에는 전설이 있어... Little endian도 Big Endian으로 알아서 처리해준다고 함

--- src/flying/bafallo/util/EndianUtil.java
+++ src/flying/bafallo/util/EndianUtil.java
@@ -6,27 +6,27 @@ import java.nio.ByteOrder;
 public class EndianUtil {
 	
 	public static int byteArrayToInt(byte[] b) {
-	    final ByteBuffer bb = ByteBuffer.wrap(b);
-	    bb.order(ByteOrder.LITTLE_ENDIAN);
+	    ByteBuffer bb = ByteBuffer.wrap(b);
+//	    bb.order(ByteOrder.LITTLE_ENDIAN);
 	    return bb.getInt();
 	}
 
 	public static byte[] intToByteArray(int i) {
-	    final ByteBuffer bb = ByteBuffer.allocate(Integer.SIZE / Byte.SIZE);
-	    bb.order(ByteOrder.LITTLE_ENDIAN);
+	    ByteBuffer bb = ByteBuffer.allocate(Integer.SIZE / Byte.SIZE);
+//	    bb.order(ByteOrder.LITTLE_ENDIAN);
 	    bb.putInt(i);
 	    return bb.array();
 	}
 	
 	public static long byteArrayToLong(byte[] b) {
-	    final ByteBuffer bb = ByteBuffer.wrap(b);
-	    bb.order(ByteOrder.LITTLE_ENDIAN);
+	    ByteBuffer bb = ByteBuffer.wrap(b);
+//	    bb.order(ByteOrder.LITTLE_ENDIAN);
 	    return bb.getLong();
 	}
 
 	public static byte[] longToByteArray(long l) {
-	    final ByteBuffer bb = ByteBuffer.allocate(Long.SIZE / Byte.SIZE);
-	    bb.order(ByteOrder.LITTLE_ENDIAN);
+	    ByteBuffer bb = ByteBuffer.allocate(Long.SIZE / Byte.SIZE);
+//	    bb.order(ByteOrder.LITTLE_ENDIAN);
 	    bb.putLong(l);
 	    return bb.array();
 	}
